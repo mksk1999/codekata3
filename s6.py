f6ee43b7e3a59921a6cba382e2e4d21b24504c57
saikumar <51634270+mksk1999@users.noreply.github.com>
2019-07-19 17:22:06 +0530

    Create s6.py

---
+++ s6.py
@@ -0,0 +1,8 @@
+x,y=map(int,input().split())
+l=list(map(int,input().split()[:x]))
+a=[]
+for i in range(y):
+  u,v=list(map(int,input().split()))
+  a.append(min(l[u-1:v]))
+for i in a:
+  print(i)
